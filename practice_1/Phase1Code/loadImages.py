6d51761e0e9d650a1f03a4fc851885241f8b46bc
Alejandro Rios <jassoalex97@hotmail.com>
2020-02-24 19:00:23 -0600

    Starting with some lines in load image

--- practice_1/Phase1Code/loadImages.py
+++ practice_1/Phase1Code/loadImages.py
@@ -8,7 +8,11 @@ kv_labels = KeyValue.SqliteKeyValue("sqlite_labels.db","labels",sortKey=True)
 kv_images = KeyValue.SqliteKeyValue("sqlite_images.db","images")
 
 # Process Logic.
-
+line = ParseTriple.ParseTriples('./dataset/images.ttl')
+for i in range(1000):
+    triple = line.getNext()
+    if triple[1] == 'http://xmlns.com/foaf/0.1/depiction':
+        kv_images.put(triple[0], triple[2])
 
 # Close KeyValues Storages
 kv_labels.close()
