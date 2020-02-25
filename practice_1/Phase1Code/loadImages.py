faf68020aa91f5d991bf7394fc1ca2a091a832f9
Alejandro Rios <jassoalex97@hotmail.com>
2020-02-24 21:18:33 -0600

    More changes for loader of labels

--- practice_1/Phase1Code/loadImages.py
+++ practice_1/Phase1Code/loadImages.py
@@ -8,11 +8,19 @@ kv_labels = KeyValue.SqliteKeyValue("sqlite_labels.db","labels",sortKey=True)
 kv_images = KeyValue.SqliteKeyValue("sqlite_images.db","images")
 
 # Process Logic.
-line = ParseTriple.ParseTriples('./dataset/images.ttl')
-for i in range(1000):
-    triple = line.getNext()
-    if triple[1] == 'http://xmlns.com/foaf/0.1/depiction':
-        kv_images.put(triple[0], triple[2])
+imageLine = ParseTriple.ParseTriples('./dataset/images.ttl')
+labelLine = ParseTriple.ParseTriples('./dataset/labels_en.ttl')
+
+print(labelLine.getNext()[2].split()) # Para obtener las palabras
+
+# for i in range(1000):
+#     imageTriple = imageLine.getNext()
+#     if imageTriple[1] == 'http://xmlns.com/foaf/0.1/depiction':
+#         kv_images.put(imageTriple[0], imageTriple[2])
+
+#     labelTriple = labelLine.getNext()
+#     if labelTriple[1] == 'http://www.w3.org/2000/01/rdf-schema#label':
+#         kv_labels.putSort(labelTriple[0], 0, labelTriple)
 
 # Close KeyValues Storages
 kv_labels.close()
