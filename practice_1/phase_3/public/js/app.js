74209b3ac795e4453dfb26cfa1d59ab80e497f9e
Alejandro Rios <jassoalex97@hotmail.com>
2020-03-04 16:52:56 -0600

    Phase  3 done!

--- practice_1/phase_3/public/js/app.js
+++ practice_1/phase_3/public/js/app.js
@@ -11,27 +11,34 @@ $(document).ready(function () {
     }
   });
 
-  function displayModal() {
+  async function displayModal() {
     $("#myModal").modal('show');
 
     $("#status").html("Searching...");
     $("#dialogtitle").html("Search for: " + $("#searchfield").val());
     $("#previous").hide();
     $("#next").hide();
-    $.getJSON('/search/' + $("#searchfield").val(), function (data) {
-      renderQueryResults(data);
-    });
+    let urls = [];
+    for (let word of $("#searchfield").val().split(' ')) {
+      console.log(word)
+      await $.getJSON('/search/' + word, function (data) {
+        urls.push(data.results);
+      });
+    }
+    urls = [...new Set(urls.flat())];
+    let data = {};
+    data.results = urls;
+    data.num_results = urls.length;
+    renderQueryResults(data);
   }
 
   let imagesUrls = [];
   let pageIndex = 0;
 
   $("#next").click(function (e) {
-    pageIndex ++;
+    pageIndex++;
     let toRender = imagesUrls.length - pageIndex * 4;
 
-    
-    
     $('#url-0').attr("src", '');
     $('#url-1').attr("src", '');
     $('#url-2').attr("src", '');
@@ -39,25 +46,47 @@ $(document).ready(function () {
 
     for (let i = pageIndex * 4; i < imagesUrls.length; i++) {
       console.log(i, imagesUrls)
-      $(`#url-${i - 4}`).attr("src", imagesUrls[i]);
+      $(`#url-${i - pageIndex * 4}`).attr("src", imagesUrls[i]);
     }
 
     $("#previous").show();
-    if(toRender < 4) $(this).hide();
+    if (toRender < 4) {
+      $(this).hide();
+    }
 
   });
 
   $("#previous").click(function (e) {
+    pageIndex--;
+
+    $('#url-0').attr("src", '');
+    $('#url-1').attr("src", '');
+    $('#url-2').attr("src", '');
+    $('#url-3').attr("src", '');
+
+    for (let i = pageIndex * 4; i < (pageIndex + 1) * 4; i++) {
+      console.log(i, imagesUrls)
+      $(`#url-${i - pageIndex * 4}`).attr("src", imagesUrls[i]);
+    }
+
+    if (imagesUrls.length > 4) {
+      $("#next").show();
+    }
+
+    if (pageIndex == 0) {
+      $(this).hide();
+    }
 
   });
 
   function renderQueryResults(data) {
     imagesUrls = [];
+    pageIndex = 0;
     if (data.error != undefined) {
       $("#status").html("Error: " + data.error);
     } else {
       $("#status").html("" + data.num_results + " result(s)");
-      
+
       let toRender = data.num_results >= 4 ? 4 : data.num_results;
       imagesUrls = data.results;
 
